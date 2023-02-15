0ed06643d17cbd9e939f4cf5bac45a2c76f02745
jmorsan <105159568+jmorsan@users.noreply.github.com>
2023-02-15 20:19:04 +0100

    Fin tarea query methods

--- src/main/java/es/iesjandula/QueryResponse/Consultas.java
+++ src/main/java/es/iesjandula/QueryResponse/Consultas.java
@@ -38,87 +38,124 @@ public class Consultas
 
     public void hacerConsultas()
     {
+        /**
+            System.out.println();
+            System.out.println("Alumno con Nombre 'Juan' :");
+            for(Alumno alumno:alumnoRepository.findByNombre("Juan"))
+            {
+                System.out.println(alumno);
+            }
+
+            System.out.println();
+            System.out.println("Grados que contengan la palabra 'Ingenieria' :");
+            for(Grado grado: gradoRepository.findByNombreContaining("Ingenieria"))
+            {
+                System.out.println(grado);
+            }
+
+            System.out.println();
+            System.out.println("Asignatura que tienen mas de 7 creditos :");
+            for(Asignatura asignatura: asignaturaRepository.findByCreditosGreaterThan(7))
+            {
+                System.out.println(asignatura);
+            }
+
+            System.out.println();
+            System.out.println("Departamentos que empiezan por la letra 'E' ignorando mayusculas  :");
+            for(Departamento departamento: departamentoRepository.findByNombreIgnoreCaseIsStartingWith("E"))
+            {
+                System.out.println(departamento);
+            }
+
+
+
+            System.out.println();
+            System.out.println("Top 10 Asignaturas ordenadas desc por creditos (Los 10 Asignaturas que menos creditos tienen) :");
+            for(Asignatura asignatura: asignaturaRepository.findTop10ByOrderByCreditosAsc())
+            {
+                System.out.println(asignatura);
+            }
+
+
+            System.out.println();
+            System.out.println("Asignaturas obligatoria pertenecientes al segundo curso :");
+            System.out.println(asignaturaRepository.findTop1ByTipoAndCurso("obligatoria",2));
+
+            for(AsignaturaResponse asignaturaResponse: asignaturaRepository.findNombreTop1ByTipoAndCurso("obligatoria",2))
+            {
+                System.out.println(asignaturaResponse);
+            }
+
+
+
+
+            System.out.println();
+            System.out.println("Contar el numero de asignaturas tipo basica :");
+
+            System.out.println(asignaturaRepository.countByTipo("básica"));
+
+            System.out.println();
+            System.out.println("Asignatura que imparte el profesor Fahey(id 9) :");
+            for(Asignatura asignatura: asignaturaRepository.findByProfesorIdId(9L))
+            {
+                System.out.println(asignatura);
+            }
+
+            System.out.println();
+            System.out.println("Asignaturas pertenecientes al segundo cuatrimestre del tercer curso :");
+            for(Asignatura asignatura: asignaturaRepository.findByCuatrimestreAndCurso(2,3))
+            {
+                System.out.println(asignatura);
+            }
+
+            System.out.println();
+            System.out.println("Alumnos que no han registrado el telefono :");
+            for(Alumno alumno:alumnoRepository.findByTelefono(" "))
+            {
+                System.out.println(alumno);
+            }
+
+            System.out.println();
+            System.out.println("Alumnos cuyo dni termina con la letra 'K' :");
+            for(Alumno alumno:alumnoRepository.findByNifEndsWith("K"))
+            {
+                System.out.println(alumno);
+            }
+         **/
+
         System.out.println();
-        System.out.println("Alumno con Nombre 'Juan' :");
-        for(Alumno alumno:alumnoRepository.findByNombre("Juan"))
+        System.out.println("Los alumnos y sus asignaturas matriculadas");
+        for(Object[] tabla : asignaturaRepository.listaAlumnoAsignatura())
         {
-            System.out.println(alumno);
+            System.out.println("Nombre alumno: " + tabla[0] + "--> Nombre asignatura: " + tabla[1] );
         }
 
         System.out.println();
-        System.out.println("Grados que contengan la palabra 'Ingenieria' :");
-        for(Grado grado: gradoRepository.findByNombreContaining("Ingenieria"))
+        System.out.println("Alumnos que menos asignaturas están matriculado");
+        for(Object[] tabla : asignaturaRepository.listaAlumnoMenosAsignatura())
         {
-            System.out.println(grado);
+            System.out.println(" Alumno: " + tabla[0] +  "--> Numero asignaturas: "+ tabla[1] );
         }
 
         System.out.println();
-        System.out.println("Asignatura que tienen mas de 7 creditos :");
-        for(Asignatura asignatura: asignaturaRepository.findByCreditosGreaterThan(7))
+        System.out.println("Cantidad de alumnos matriculados en cada asignatura");
+        for(Object[] tabla : asignaturaRepository.listaAlumnoPorAsignatura())
         {
-            System.out.println(asignatura);
+            System.out.println(" Nombre asignatura: " + tabla[0] +  "--> Numero alumnos matriculados: "+ tabla[1] );
         }
 
         System.out.println();
-        System.out.println("Departamentos que empiezan por la letra 'E' ignorando mayusculas  :");
-        for(Departamento departamento: departamentoRepository.findByNombreIgnoreCaseIsStartingWith("E"))
+        System.out.println("Cantidad de profesores en cada departamento");
+        for(Object[] tabla : asignaturaRepository.listaAlumnoPorAsignatura())
         {
-            System.out.println(departamento);
-        }
-
-
-
-        System.out.println();
-        System.out.println("Top 10 Asignaturas ordenadas desc por creditos (Los 10 Asignaturas que menos creditos tienen) :");
-        for(Asignatura asignatura: asignaturaRepository.findTop10ByOrderByCreditosAsc())
-        {
-            System.out.println(asignatura);
-        }
-
-
-        System.out.println();
-        System.out.println("Asignaturas obligatoria pertenecientes al segundo curso :");
-        System.out.println(asignaturaRepository.findTop1ByTipoAndCurso("obligatoria",2));
-        /*
-        for(AsignaturaResponse asignaturaResponse: asignaturaRepository.findNombreTop1ByTipoAndCurso("obligatoria",2))
-        {
-            System.out.println(asignaturaResponse);
-        }
-        */
-
-
-
-        System.out.println();
-        System.out.println("Contar el numero de asignaturas tipo basica :");
-
-        System.out.println(asignaturaRepository.countByTipo("básica"));
-
-        System.out.println();
-        System.out.println("Asignatura que imparte el profesor Fahey(id 9) :");
-        for(Asignatura asignatura: asignaturaRepository.findByProfesorIdId(9L))
-        {
-            System.out.println(asignatura);
+            System.out.println(" Nombre departamento: " + tabla[0] +  "--> Numero profesores: "+ tabla[1] );
         }
 
         System.out.println();
-        System.out.println("Asignaturas pertenecientes al segundo cuatrimestre del tercer curso :");
-        for(Asignatura asignatura: asignaturaRepository.findByCuatrimestreAndCurso(2,3))
+        System.out.println("Alumnos y curso que pertenece");
+        for(Object[] tabla : asignaturaRepository.listaAlumnosCurso())
         {
-            System.out.println(asignatura);
-        }
-
-        System.out.println();
-        System.out.println("Alumnos que no han registrado el telefono :");
-        for(Alumno alumno:alumnoRepository.findByTelefono(" "))
-        {
-            System.out.println(alumno);
-        }
-
-        System.out.println();
-        System.out.println("Alumnos cuyo dni termina con la letra 'K' :");
-        for(Alumno alumno:alumnoRepository.findByNifEndsWith("K"))
-        {
-            System.out.println(alumno);
+            System.out.println(" Nombre alumno: " + tabla[0] +  "--> Curso: "+ tabla[1]+"/"+tabla[2] );
         }
     }
 }
--- src/main/java/es/iesjandula/repositories/IAsignaturaRepository.java
+++ src/main/java/es/iesjandula/repositories/IAsignaturaRepository.java
@@ -25,16 +25,26 @@ public interface IAsignaturaRepository extends JpaRepository<Asignatura,Long>
     List<Asignatura> findByCuatrimestreAndCurso(int cuatrimestre,int curso);
 
     //Los alumnos y sus asignaturas matriculadas
-    @Query("select alum.nombre, asig.nombre from alumno alum join matricula mat on alum.id = mat.id_alumno join asignatura asig on asig.id = mat.id_asignatura")
-    List<MyResponse>listaAlumnoAsignatura();
+    @Query(value = "select alum.nombre, asig.nombre from alumno alum join matricula mat on alum.id = mat.id_alumno join asignatura asig on asig.id = mat.id_asignatura",nativeQuery = true)
+    List<Object[]>listaAlumnoAsignatura();
 
     //Alumnos que menos asignaturas está matriculado
     @Query(value = "select alum.nombre, COUNT(mat.id_asignatura) AS cantidadAsignaturas from alumno alum join matricula mat on alum.id = mat.id_alumno join asignatura asig on asig.id = mat.id_asignatura GROUP BY  mat.id_alumno HAVING  COUNT(mat.id_asignatura) = ( select COUNT(mat.id_asignatura) AS cantidadAsignaturas from alumno alum join matricula mat on alum.id = mat.id_alumno join asignatura asig on asig.id = mat.id_asignatura GROUP BY  mat.id_alumno ORDER BY mat.id_alumno DESC LIMIT 1)", nativeQuery = true)
-    List<MyResponse>listaAlumnoNumAsignatura();
+    List<Object[]>listaAlumnoMenosAsignatura();
 
     //Cantidad de alumnos matriculados en cada asignatura
     @Query(value = "select asig.nombre, COUNT(mat.id_asignatura) AS cantidadAlumnosMatriculados from alumno alum join matricula mat on alum.id = mat.id_alumno join asignatura asig on asig.id = mat.id_asignatura GROUP BY  mat.id_asignatura ORDER BY cantidadAlumnosMatriculados", nativeQuery = true)
-    List<MyResponse>listaAlumnoNumAsignatura();
+    List<Object[]>listaAlumnoPorAsignatura();
+
+    //Cantidad de profesores en cada departamento
+    @Query(value="select dpt.nombre, COUNT(prof.id) AS cantidadProfesores from departamento dpt join profesor prof on dpt.id = prof.id_departamento_id  GROUP BY  dpt.id ORDER BY cantidadProfesores",nativeQuery = true)
+    List<Object[]>listaDepartamentoProfesores();
+
+    //Alumnos y curso que pertenece
+    @Query(value = "select alum.nombre, cur.anyo_fin,cur.anyo_inicio from alumno alum join matricula mat on alum.id = mat.id_alumno join curso cur on cur.id = mat.id_curso group by mat.id_alumno",nativeQuery = true)
+    List<Object[]>listaAlumnosCurso();
+
+
 
 
 
